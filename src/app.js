e2e080d17c423db17b2862dcab1f0675ff5dd031
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-01-22 17:52:42 -0600

    configurando cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
@@ -8,6 +9,7 @@ const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 // Routes
